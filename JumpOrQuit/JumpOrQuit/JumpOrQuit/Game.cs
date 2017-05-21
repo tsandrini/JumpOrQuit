cec40623fc6f5be2b9908face97534c947420ee5
tsandrini <WareCzech@gmail.com>
2017-05-21 11:40:36 +0200

    update

---
+++ JumpOrQuit/JumpOrQuit/JumpOrQuit/Game.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+using JumpOrQuit.Classes;
+using JumpOrQuit.Components;
+
+// Aliases
+using GameWindow = JumpOrQuit.Classes.GameWindow; // Override the default game window
+
+namespace JumpOrQuit
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        protected KeyboardState keys, lastKey;
+        protected MouseState mouse, lastMouse;
+        protected GameWindow loadingScreen, menuWindow, ingameWindow;
+
+        public Game()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- JumpOrQuit/JumpOrQuit/JumpOrQuit/Program.cs
+++ JumpOrQuit/JumpOrQuit/JumpOrQuit/Program.cs
@@ -10,7 +10,7 @@ namespace JumpOrQuit
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (Game game = new Game())
             {
                 game.Run();
             }
